8b1653a930eacb8ea13cea9ecde1a5ee51bb2eeb
david <david@cninone.com>
2017-11-28 17:44:15 +0800

    test scrolling world

--- test/js/util.js
+++ test/js/util.js
@@ -1,12 +1,11 @@
+const world_width = 512;
+const world_height = 512;
 let designed_world ={
     1:{2:0}
     //...
 }
-let small_world_array = [
-    //loaded tile layers as small world
-];
-let loaded_world_map = {};
-function get_adjacent_indexes(x, y){
+
+function cur_worlds(x, y){
     return [
         [x, y],
         [x, y-1],
@@ -19,23 +18,46 @@ function get_adjacent_indexes(x, y){
         [x-1, y-1]
     ];
 }
-function load_world(x, y){
-    let small_world = {};
-    small_world.x = Math.floor(x / 1024);
-    small_world.y = Math.floor(y / 1024);
-    let adjacent_sw_array = get_adjacent_indexes(small_world.x, small_world.y);
-    adjacent_sw_array.forEach( coor =>{
-        if( !loaded_world_map[ coor[0] ] ){
-            loaded_world_map[ coor[0] ] = {}
+function load_own_world(world, player){
+    let map_x = Math.floor(player.x / world_width);
+    let map_y = Math.floor(player.y / world_height);
+    if(player.last_map && player.last_map.x == map_x && player.last_map.y == map_y ){
+        return world;
+    }
+    player.last_map = {
+        x:map_x,
+        y:map_y
+    }
+    let adjacent_sw_array = cur_worlds(map_x, map_y);
+    console.log(adjacent_sw_array);
+    let loaded_map = world.loaded_world_map;
+    let small_worlds = adjacent_sw_array.map( coor =>{
+        let map_x = coor[0], map_y = coor[1];
+        if( !loaded_map[ map_x ] ){
+            loaded_map[ map_x ] = {}
         }
-        if( !loaded_world_map[coor[0]][coor[1]] ){
+        if( !loaded_map[map_x][map_y] ){
             //let's skip designed_world for now
-            if(small_world_array.length > 0){
-                let sel_index = g.randomInt(0, small_world_array.length -1);
+            if(world.candidate.length > 0){
+                let sel_index = g.randomInt(0, world.candidate.length -1);
+                let thisMap = world.candidate[sel_index].clone()
                 
-                
-                loaded_world_map[coor[0]][coor[1]] = sel_index;
+                world.addChild(thisMap)
+                thisMap.setPosition(map_x*world_width, map_y*world_height);
+                // thisMap.putCenter( g.text(`(map_x:${map_x}, map_y${map_y})`) )
+                // console.log(thisMap.x, thisMap.y, thisMap.layer);
+                loaded_map[map_x][map_y] = thisMap;                
             }
         }
+        return loaded_map[map_x][map_y];
     })
+    if(player.parent != world){
+        console.log('add player to world')
+        player.layer = 9;
+        world.add(player)
+    }
+    console.log('world.children.length:' + world.children.length);
+    console.log('world.width:' + world.width + '; world.height:' + world.height);
+    console.log('world.x:' + world.x + '; world.y:' + world.y);
+    return world;
 }
